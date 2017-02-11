7ca04ad6a6472e78a36b9b1aede809f5e9937720
Jeremy Burns <j@jaburns.net>
2017-02-11 13:00:11 -0700

    Added namespace

--- TypedSendMessage.cs
+++ TypedSendMessage.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 
-public interface Message <T>
+namespace jaburns.UnityTools
 {
-    void Receive(T param);
-}
-
-static public class TypedSendMessage
-{
-    static public void SendTypedMessage <T> (this GameObject target, T param = default(T))
+    public interface Message <T>
     {
-        var targs = target.GetComponents(typeof(Message<T>));
-        foreach (var targ in targs) {
-            (targ as Message<T>).Receive(param);
+        void Receive(T param);
+    }
+
+    static public class TypedSendMessage
+    {
+        static public void SendTypedMessage <T> (this GameObject target, T param = default(T))
+        {
+            var targs = target.GetComponents(typeof(Message<T>));
+            foreach (var targ in targs) {
+                (targ as Message<T>).Receive(param);
+            }
         }
     }
 }
